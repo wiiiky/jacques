05420e7b566acfe4b84cfab8b904aebfc02688a1
wiiiky <wiiiky@outlook.com>
2015-02-12 17:42:09 +0800

    main

---
+++ src/config.h
@@ -0,0 +1,71 @@
+/*
+ * config.h
+ * Copyright (C) 2015 Wiky L <wiiiky@outlook.com>
+ *
+ * jacques is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * jacques is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.";
+ */
+
+#ifndef __JAC_CONFIG_H__
+#define __JAC_CONFIG_H__
+
+
+#define PROGRAME_NAME   "jacques"
+#define PROGRAME_VERSION    "0.01"
+
+/*********************** /etc *****************************/
+#ifndef CONFIG_LOCATION
+#define CONFIG_LOCATION ("/etc/" PROGRAME_NAME)
+#endif
+
+
+#ifndef CONFIG_FILENAME
+#define CONFIG_FILENAME (PROGRAME_NAME ".conf")
+#endif
+
+
+#define CONFIG_FILEPATH (CONFIG_LOCATION "/" CONFIG_FILENAME)
+
+
+/********************* /var/run **************************/
+#ifndef RUNTIME_LOCATION
+#define RUNTIME_LOCATION ("/var/run/" PROGRAME_NAME)
+#endif
+
+
+#ifndef PID_FILENAME
+#define PID_FILENAME (PROGRAME_NAME ".pid")
+#endif
+
+#define PID_FILEPATH (RUNTIME_LOCATION "/" PID_FILENAME)
+
+
+/********************* /var/log ****************************/
+#ifndef LOG_LOCATION
+#define LOG_LOCATION ("/var/log/" PROGRAME_NAME)
+#endif
+
+
+#ifndef LOG_FILENAME
+#define LOG_FILENAME (PROGRAME_NAME ".log")
+#endif
+
+#ifndef ERR_FILENAME
+#define ERR_FILENAME (PROGRAME_NAME ".err")
+#endif
+
+#define LOG_FILEPATH (LOG_LOCATION "/" LOG_FILENAME)
+#define ERR_FILEPATH (LOG_LOCATION "/" ERR_FILENAME)
+
+
+#endif
--- src/main.c
+++ src/main.c
@@ -2,25 +2,104 @@
 /*
  * main.c
  * Copyright (C) 2015 Wiky L <wiiiky@outlook.com>
- * 
+ *
  * jacques is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * jacques is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.";
  */
 
+#include "config.h"
+#include <jlib/jlib.h>
 #include <stdio.h>
-int main()
+#include <stdlib.h>
+#include <getopt.h>
+#include <unistd.h>
+#include <locale.h>
+#include <libintl.h>
+
+#define _(string)   gettext(string)
+
+
+static struct option long_options[] = {
+    {"version", 0, 0, 'v'},
+    {"help", 0, 0, 'h'},
+    {0, 0, 0, 0},
+};
+
+static inline void show_help(void);
+static inline void show_version(void);
+
+static inline void start_jacques(void);
+static inline void stop_jacques(void);
+
+int main(int argc, char *argv[])
 {
-    printf("Hello world\n");
-    return (0);
+    setlocale(LC_ALL,"");
+    int opt;
+    while ((opt = getopt_long(argc, argv, "vh", long_options, NULL)) != -1) {
+        switch (opt) {
+        case 'v':
+            show_version();
+            break;
+        case 'h':
+            show_help();
+            break;
+        default:
+            break;
+        }
+    }
+    if (optind != argc - 1) {
+        show_help();
+    }
+    const char *cmd = argv[optind];
+    if (j_strcmp0(cmd, "start") == 0) {
+        start_jacques();
+    } else if (j_strcmp0(cmd, "stop") == 0) {
+        stop_jacques();
+    }
+    printf(_("error: unknown command \'%s\'\n\n"), cmd);
+    show_help();
+
+    return 0;
 }
 
+
+static inline void show_help(void)
+{
+    printf(_("%s version: %s/%s\n"),
+           PROGRAME_NAME, PROGRAME_NAME, PROGRAME_VERSION);
+    printf(_("Usage: %s start|stop [options]\n\n"), PROGRAME_NAME);
+    printf(_("Options:\n"));
+    printf(_("  -h\t: show this help\n"));
+    printf(_("  -v\t: show version and exit\n"));
+    printf("\n");
+    exit(0);
+}
+
+static inline void show_version(void)
+{
+    printf(_("%s version: %s/%s\n"),
+           PROGRAME_NAME, PROGRAME_NAME, PROGRAME_VERSION);
+    exit(0);
+}
+
+static inline void start_jacques(void)
+{
+    printf("started!\n");
+    exit(0);
+}
+
+static inline void stop_jacques(void)
+{
+    printf("stopped!\n");
+    exit(0);
+}
