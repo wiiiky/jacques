9685df9cccab16266e7ce3b31f90d90d8f8d2f03
wiiiky <wiiiky@outlook.com>
2015-09-02 17:12:34 +0800

    echo服务效率低是测试程序的问题#3

--- tests/flood.py
+++ tests/flood.py
@@ -1,65 +1,41 @@
 #!/usr/bin/env python3
 
 
-import threading
 import socket
-import random
-import string
+import select
 import time
-import sys
 
 PORT = 43212
-WORKER_COUNT = 6
+pfd = select.epoll()
 
-if len(sys.argv) >= 2:
-    PORT = int(sys.argv[1])
-if len(sys.argv) >= 3:
-    WORKER_COUNT = int(sys.argv[2])
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+s.connect(('localhost', PORT))
 
-print('flood port %d' % PORT)
+pfd.register(s, select.EPOLLIN|select.EPOLLOUT)
 
-
-def random_string():
-    n = int(random.random()*3000)
-    s = '1'
-    for i in range(n):
-        s += random.choice(string.ascii_letters)
-    return s
-
-
-class Worker(threading.Thread):
-
-    def run(self):
-        global PORT
-        self.count = 0
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-        try:
-            s.connect(('localhost', PORT))
-        except:
-            return
-        for i in range(1000):
-            data = random_string().encode('ascii')
-            self.count += len(data)
-            s.sendall(data)
-            s.recv(4024)
-        s.close()
-
-    def join(self):
-        super(Worker, self).join()
-        return self.count
-
-workers = []
-start = time.time()
-for i in range(WORKER_COUNT):
-    w = Worker()
-    w.start()
-    workers.append(w)
+s.sendall(b'hello world')
 
 count = 0
-for i in range(WORKER_COUNT):
-    count += workers[i].join()
+
+start = time.time()
+running = True
+
+size= 1024
+
+while running:
+    events = pfd.poll()
+    for fileno, event in events:
+        if event & select.EPOLLIN:
+            count+=len(s.recv(size))
+            if count > 1024*1024*50:
+                running=False
+        if event & select.EPOLLOUT:
+            s.sendall(('a'*size).encode('ascii'))
+        if event & select.EPOLLHUP or event & select.EPOLLERR:
+            break;
 
 end = time.time()
 
-diff = end-start;
-print('time: %s, total: %s KB/S' % (diff, count/diff/1024))
+duration = end-start
+
+print('time: %s -- %s MB/S' % (duration, count/1024/1024/duration))
